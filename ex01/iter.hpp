eca28496c128e39364d26790c20268e54da20c18
Milos Mitkovic <mitkovic_m@hotmail.com>
2026-02-26 11:12:11 +0100

    Add func definition on ex01

---
+++ ex01/iter.hpp
@@ -0,0 +1,12 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <iostream>
+
+//void iter(char *arr, size_t lenArr, int (*func)(int))
+
+template <typename T, typename F> void iter(T &arr, const size_t lenArr, T(*func)(T)) {
+
+}
+
+#endif
